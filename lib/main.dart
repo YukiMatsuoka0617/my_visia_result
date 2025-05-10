f986d76e2fb1a2c8c7fa3a38284192a0ffde2301
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2025-05-11 00:23:41 +0900

    refactor: separated HomeScreen from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_result_visia/chart_display_area.dart';
-import 'package:flutter_application_result_visia/chart_select_area.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:flutter_application_result_visia/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,128 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<List<double>> allChartData = [
-    [-37, -39, -41, 47, 46, -24, 4],
-    [-39, 43, -41, 44, 45, -31, 49],
-    [-41, -4, 8, 23, 17, -41, -25],
-    [-30, 5, 15, 1, 26, -41, -10],
-    [-36, -39, -41, 29, 38, -41, -41],
-  ];
-  final List<String> labelData = [
-    'Spots',
-    'UV Spots',
-    'Brown Spots',
-    'Wrinkles',
-    'Texture',
-    'Pores',
-    'Porphyrins',
-  ];
-  final List<String> dateData = [
-    '2025/04/20',
-    '2025/03/14',
-    '2025/02/16',
-    '2025/01/10',
-    '2024/12/02',
-  ];
-
-  final PageController _pageController = PageController();
-  int _currentPage = 0;
-  bool isDailyResultShown = true;
-  var selectLabelIndex = 0;
-
-  String selectedCategory = 'All';
-  String selectedSubCategory = 'Spots';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Expanded(
-              child: ChartDisplayArea(
-                isDailyResultShown: isDailyResultShown,
-                currentPage: _currentPage,
-                selectLabelIndex: selectLabelIndex,
-                dateData: dateData,
-                labelData: labelData,
-                allChartData: allChartData,
-                pageController: _pageController,
-                onPageChanged: (value) {
-                  setState(() {
-                    _currentPage = value;
-                  });
-                },
-              ),
-            ),
-            if (isDailyResultShown)
-              SmoothPageIndicator(
-                controller: _pageController,
-                count: allChartData.length,
-                effect: const WormEffect(
-                  dotHeight: 10,
-                  dotWidth: 10,
-                  activeDotColor: Colors.deepPurple,
-                ),
-              ),
-            const SizedBox(height: 16),
-            ChartSelectArea(
-              selected: selectedCategory,
-              onSelectionChanged: (newSelection) {
-                setState(() {
-                  selectedCategory = newSelection.first;
-                  isDailyResultShown = (selectedCategory == 'All');
-                });
-              },
-              showSubCategory: selectedCategory == 'Category',
-              selectedSubCategory: selectedSubCategory,
-              onSubCategoryChanged: (subSelection) {
-                setState(() {
-                  selectedSubCategory = subSelection.first;
-                });
-              },
-              subCategories: labelData,
-              onLabelSelected: (index) {
-                setState(() {
-                  selectLabelIndex = index;
-                  isDailyResultShown = false;
-                });
-              },
-              onAllSelected: () {
-                setState(() {
-                  _currentPage = 0;
-                  isDailyResultShown = true;
-                });
-              },
-            ),
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-      ),
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text('My VISIA History'),
+          ),
+          body: HomeScreen(),
+        ));
   }
 }
