8328161fbad45eaa42c2bcf7a6147c05dc344488
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2025-05-11 00:30:18 +0900

    Chage Color

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
           useMaterial3: true,
         ),
         home: Scaffold(
           appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            backgroundColor: Colors.blueAccent,
             title: Text('My VISIA History'),
           ),
           body: HomeScreen(),
